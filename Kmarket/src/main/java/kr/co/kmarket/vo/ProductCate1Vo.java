e0e862fe9cf3a250f49f7a0b72652abe917def14
D <88361198+lollolzz@users.noreply.github.com>
2021-11-02 21:29:32 +0900

    로그인 기능 구현

--- Kmarket/src/main/java/kr/co/kmarket/vo/ProductCate1Vo.java
+++ Kmarket/src/main/java/kr/co/kmarket/vo/ProductCate1Vo.java
@@ -7,4 +7,18 @@ public class ProductCate1Vo {
 
 	private String cate1;
 	private String title;
+	public String getCate1() {
+		return cate1;
+	}
+	public void setCate1(String cate1) {
+		this.cate1 = cate1;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
 }

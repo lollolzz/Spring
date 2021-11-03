68eb66033d58590ddbfda10f182b3718c67b7561
D <88361198+lollolzz@users.noreply.github.com>
2021-11-03 21:17:29 +0900

    판매자 상품등록(파일 업로드 까지 완료)

--- Kmarket/src/main/java/kr/co/kmarket/vo/ProductOrderReviewVo.java
+++ Kmarket/src/main/java/kr/co/kmarket/vo/ProductOrderReviewVo.java
@@ -13,50 +13,5 @@ public class ProductOrderReviewVo {
 	private String ip;
 	private String rdate;
 	
-	public int getReviewId() {
-		return reviewId;
-	}
-	public void setReviewId(int reviewId) {
-		this.reviewId = reviewId;
-	}
-	public int getProductCode() {
-		return productCode;
-	}
-	public void setProductCode(int productCode) {
-		this.productCode = productCode;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getUid() {
-		return uid;
-	}
-	public void setUid(String uid) {
-		this.uid = uid;
-	}
-	public String getRating() {
-		return rating;
-	}
-	public void setRating(String rating) {
-		this.rating = rating;
-	}
-	public String getIp() {
-		return ip;
-	}
-	public void setIp(String ip) {
-		this.ip = ip;
-	}
-	public String getRdate() {
-		return rdate;
-	}
-	public void setRdate(String rdate) {
-		this.rdate = rdate;
-	}
-	
-	
-	
 
 }

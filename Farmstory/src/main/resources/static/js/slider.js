48812b861f506b78b9f71a31aef3653a74c059ea
lollolzz <lollolzz1018@gmail.com>
2021-12-12 23:48:56 +0900

    aws배포 완료

--- Farmstory/src/main/resources/static/js/slider.js
+++ Farmstory/src/main/resources/static/js/slider.js
@@ -1,12 +1,13 @@
+// index 페이지에 작성하여도 되지만 js폴더 안의 파일을 가져올 수 있는지 확인 하기 위하여 해봄~
  $(function(){
 
-            $('.slider > ul').bxSlider({
-                slideWidth: 980,
-                pager: false,
-                controls: false,
-                auto: true
-            });
+    $('.slider > ul').bxSlider({
+        slideWidth: 980,
+        pager: false,
+        controls: false,
+        auto: true
+    });
 
-            $('#tabs').tabs();
+    $('#tabs').tabs();
 
-        });
+});

421639a90ea344bd0f8c37f002d2cf8cf5e2ae2f
lollolzz <lollolzz1018@gmail.com>
2022-03-09 23:28:31 +0900

    http, maven

--- blog/src/main/java/com/cos/blog/test/HttpControlloerTest.java
+++ blog/src/main/java/com/cos/blog/test/HttpControlloerTest.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -13,25 +14,33 @@ import org.springframework.web.bind.annotation.RestController;
 //  사용자가 요청 -> 응답 (HTML 파일 ) -> @Controller 
 public class HttpControlloerTest {
 	
-	
+	private static final String TAG = "HttpControlloerTest :";
+
 	// http://localhost:8080/http/get(select)
 	@GetMapping("/http/get")
-	public String getTest() {
-//		return type은 문자열이다 
-		return "get 요청";
+	public String getTest(Member m) {
+		// id=1&username=ssar&password=1234&email=ssar@nate.com 이 값을 Member에 넣어준다  // MessageConverter (스프링부트)
+		//return type은 문자열이다 
+		System.out.println(TAG+"getter :" + m.getId());
+		m.setId(5000);
+		System.out.println(TAG+"setter : "+m.getId());
+		
+		return "get 요청: " + m.getId()+" , "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
+		
 	}
+	
 	// 인터넷 브라우저 요청은 무조건 get요청 뿐이 되지 않는다!!!
 	// 나머지는postman을 사용해서 확인해보자 
-	// http://localhost:8080/http/post(insert)
-	@PostMapping("/http/post")
-	public String postTest() {
-		return "post 요청";
+	// http://localhost:8080/http/post(insert) / 데이터를 추가 해라 
+	@PostMapping("/http/post") // text/plain, application/json 
+	public String postTest(@RequestBody Member m) { // MessageConverter (스프링부트)
+		return "post 요청 :  " + m.getId()+" , "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
 	}
 	
 	// http://localhost:8080/http/put(update)
 	@PutMapping("/http/put")
-	public String putTest() {
-		return "put 요청";
+	public String putTest(@RequestBody Member m) {
+		return "put 요청 :" + m.getId()+" , "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
 	}
 	
 	// http://localhost:8080/http/delete(delete)
